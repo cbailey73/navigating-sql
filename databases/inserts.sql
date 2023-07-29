ee0874a4476700362474e7cbaf7a299313fbf614
cbailey73 <123669370+cbailey73@users.noreply.github.com>
2023-07-29 15:33:10 -0500

    fixed title name

--- databases/inserts.sql
+++ databases/inserts.sql
@@ -1,4 +1,4 @@
--- Sample data for the departments table
+-- Seed data for all tables
 INSERT INTO departments (title) VALUES
 ('Sales'),
 ('Marketing'),
@@ -18,17 +18,3 @@ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Doe', 5, null),
 ('Bob', 'Douglas', 3, 1);
 
-
-
-
--- INSERT INTO roles (title, salary, department_id, department_title) VALUES
--- ('Sales Manager', 60000, 1, 'Sales'),
--- ('Software Engineer', 80000, 3, 'Engineering'),
--- ('Marketing Specialist', 70000, 2, 'Marketing'),
--- ('Accountant', 75000, 4, 'Finance');
-
--- INSERT INTO employees (first_name, last_name, role_id, role_title, department_id, department_title, manager_names) VALUES
--- ('John', 'Doe', 1, 'Sales Assistant', 1, 'Sales', 'Bob'),
--- ('Emily', 'Williams', 3, 'Marketing Specialist', 2, 'Marketing', 'Sally'),
--- ('Richard', 'Rich', 4, 'Accountant', 4, 'Finance', 'Horace'),
--- ('Sarah', 'Green', 2, 'Software Engineer', 3, 'Engineering');
